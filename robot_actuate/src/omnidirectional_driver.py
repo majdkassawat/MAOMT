7166399c49686597b611830e82b0ec14d7f90906
majdkassawat <majd.kassawat@gmail.com>
2019-07-07 17:35:31 +0200

    adding robot actuate for omnidirectional movement and force sensor interface

---
+++ robot_actuate/src/omnidirectional_driver.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.7
+import rospy
+import math
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+
+
+
+# constraints of the motors velocities
+maximum_abs_speed = 1000
+maximum_distance_const = 40 # This is used to normalize the rotation velocity
+
+#Information of the chosen reference frame (in cm)
+lambda_ref = 0
+x_ref = -8
+z_ref = 5
+
+#Here we need to indicate the Thetai,Zi,Xi,rotation_direction for every wheel.
+theta = [0,-120,120]
+z = [7, -3.5, -3.5] #in cm
+x = [0, 6, -6] #in cm
+rot_dir = [1, 1, 1]
+
+#Equations parameters to determine the distances from center
+a=[0,0,0]
+b = -1
+c=[0,0,0]
+d=[0,0,0]
+
+#Inputs for Omnidirectional drivers from transmitor
+Vtz_manual = 0
+Vtx_manual = 0
+W_manual = 0
+
+#Inputs for Omnidirectional drivers automated
+Vtz_automated = 0
+Vtx_automated = 0
+W_automated = 0
+
+
+
+
+# def rad(deg):
+#   rad = deg * 1000 / 57296
+#   return rad
+
+for i in range(0,3):
+    # global a,c,d
+    # print(rad(theta[i]))
+    a[i] = math.tan(math.radians(theta[i]))
+    c[i] = z[i] - a[i] * x[i]
+    d[i] = abs(a[i] * x_ref + b * z_ref + c[i]) / math.sqrt(a[i] * a[i] + b * b)
+
+
+def omnidirectional_driver(msg):
+    # global a,b,c,d,theta,z,x,rot_dir,lambda_ref,x_ref,z_ref
+    # print("from the library",msg)
+    final_vel=[0,0,0]
+    W = msg.angular.z 
+    Vtx = msg.linear.x
+    Vtz = msg.linear.y
+    for i in range(0,3):
+        # global final_vel
+        Vroti = 0
+        Vtransi = 0
+
+        Vroti = (d[i] / maximum_distance_const) * W * rot_dir[i]
+        # print(d[i])
+        Vtransi = math.cos(math.radians(theta[i]) - math.radians(lambda_ref)) * Vtz + math.sin(math.radians(theta[i]) - math.radians(lambda_ref)) * Vtx
+        Vi = Vtransi + Vroti
+        final_vel[i]=Vi
+    # print(final_vel)
+
+
+    return final_vel[0],final_vel[1],final_vel[2]
---
+++ robot_actuate/src/robot_actuate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2.7
+import rospy
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+from omnidirectional_driver import omnidirectional_driver
+
+traction_wheel_left_controller_msg = Float64()
+traction_wheel_right_controller_msg = Float64()
+wheel1_controller_msg = Float64()
+wheel2_controller_msg = Float64()
+wheel3_controller_msg = Float64()
+
+
+def cmdTwistVelCallback(msg):
+    wheel1_vel,wheel2_vel,wheel3_vel = omnidirectional_driver(msg)
+    wheel1_controller_msg.data=wheel1_vel
+    wheel2_controller_msg.data=wheel2_vel
+    wheel3_controller_msg.data=wheel3_vel
+    wheel1_controller_pub.publish(wheel1_controller_msg)
+    wheel2_controller_pub.publish(wheel2_controller_msg)
+    wheel3_controller_pub.publish(wheel3_controller_msg)
+    # print("INPUT:",msg.angular.z,msg.linear.x,msg.linear.y)
+    # print("OUTPUT:",wheel1_vel,wheel2_vel,wheel3_vel)
+    
+
+
+def cmdTractionLeftVelCallback(msg):
+    traction_wheel_left_controller_msg.data=msg.data
+    traction_wheel_left_controller_pub.publish(traction_wheel_left_controller_msg)
+
+
+def cmdTractionRightVelCallback(msg):
+    traction_wheel_right_controller_msg.data=msg.data
+    traction_wheel_right_controller_pub.publish(traction_wheel_right_controller_msg)
+
+
+
+rospy.init_node('robot_actuate_node', log_level=rospy.DEBUG)
+
+traction_wheel_left_controller_pub = rospy.Publisher('/traction_wheel_left_controller/command', Float64, queue_size=1)
+traction_wheel_right_controller_pub = rospy.Publisher('/traction_wheel_right_controller/command', Float64, queue_size=1)
+wheel1_controller_pub = rospy.Publisher('/wheel1_controller/command', Float64, queue_size=1)
+wheel2_controller_pub = rospy.Publisher('/wheel2_controller/command', Float64, queue_size=1)
+wheel3_controller_pub = rospy.Publisher('/wheel3_controller/command', Float64, queue_size=1)
+
+command_twist_vel_sub = rospy.Subscriber('/cmd_vel', Twist, cmdTwistVelCallback)
+command_traction_wheel_left_vel_sub = rospy.Subscriber('/traction_wheel_left_vel', Float64, cmdTractionLeftVelCallback)
+command_traction_wheel_right_vel_sub = rospy.Subscriber('/traction_wheel_right_vel', Float64, cmdTractionRightVelCallback)
+
+
+r = rospy.Rate(100)
+while not rospy.is_shutdown():
+    r.sleep()
