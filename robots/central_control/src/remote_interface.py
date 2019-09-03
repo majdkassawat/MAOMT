28724d2964d39c536c495be80c4b812788581bf3
majdkassawat <majd.kassawat@gmail.com>
2019-09-03 21:57:43 +0200

    added stop and sync functionalities to remote_interface

--- robots/central_control/src/remote_interface.py
+++ robots/central_control/src/remote_interface.py
@@ -3,37 +3,76 @@ import rospy
 from std_msgs.msg import Bool
 from std_msgs.msg import Float64
 from std_msgs.msg import UInt32
+import time
+import threading
+import signal
 
 
+cancel_signal = False
 freq = 40
+mode = "stop"
+sync = False
+
+# Defining Ros messages
+stop_msg = Bool()
+sync_msg = Bool()
+
+def sigint_handler(signum, frame):
+    global cancel_signal
+    cancel_signal = True
+signal.signal(signal.SIGINT, sigint_handler)
 
 
 def remote_ch1_Callback(msg):
-    print("ch1: "+str(msg.data))
+    x = 1+2
+    # print("ch1: "+str(msg.data))
 
 
 def remote_ch2_Callback(msg):
-    print("ch2: "+str(msg.data))
+    x = 1+2
+    # print("ch2: "+str(msg.data))
 
 
 def remote_ch3_Callback(msg):
-    print("ch3: "+str(msg.data))
+    x = 1+2
+    # print("ch3: "+str(msg.data))
 
 
 def remote_ch4_Callback(msg):
-    print("ch4: "+str(msg.data))
+    x = 1+2
+    # print("ch4: "+str(msg.data))
 
 
 def remote_ch5_Callback(msg):
-    print("ch5: "+str(msg.data))
+    global mode
+    if (msg.data > 0) and (msg.data <= 1200):
+        mode = "stop"
+    elif (msg.data > 1200) and (msg.data <= 1742):
+        mode = "lock"
+    elif (msg.data > 1742) and (msg.data <= 2500):
+        mode = "control"
+    else:
+        raise ValueError(
+            "the value received from the transmitter is not valid on ch5: " + str(msg.data))
+
 
 
 def remote_ch6_Callback(msg):
-    print("ch6: "+str(msg.data))
+    global sync
+    if (msg.data > 0) and (msg.data <= 1500):
+        sync = False
+    elif (msg.data > 1500) and (msg.data <= 2500):
+        sync = True
+    else:
+        raise ValueError(
+            "the value received from the transmitter is not valid on ch6: " + str(msg.data))
 
 
-t_left_vel_pub = rospy.Publisher(
-    'traction_wheel_left_vel', Float64, queue_size=10)
+
+sync_pub = rospy.Publisher(
+    '/sync', Bool, queue_size=10)
+stop_pub = rospy.Publisher(
+    '/stop_signal', Bool, queue_size=10)
 sub_remote_ch1 = rospy.Subscriber(
     'remote_transmitter/ch1', UInt32, remote_ch1_Callback)
 sub_remote_ch2 = rospy.Subscriber(
@@ -52,7 +91,28 @@ sub_remote_ch6 = rospy.Subscriber(
 # t_left_vel_pub.publish(t_left_msg)
 
 rospy.init_node('remote_interface', log_level=rospy.INFO)
-r = rospy.Rate(freq)
-while not rospy.is_shutdown():
-    # interface()
-    r.sleep()
+
+def remote_interface():
+    # print("mode: "+ str(mode)+" sync: "+ str(sync))
+    # Process stop signal
+    if mode == "stop":
+        stop_msg.data = True
+    else:
+        stop_msg.data = False
+    # Process sync signal
+    sync_msg.data = sync
+
+
+    # Publish all messages
+    stop_pub.publish(stop_msg)
+    sync_pub.publish(sync_msg)
+    # If cancel signal is not received (ctrl+c), then execute this function again respecting the frequency (here no need for ros sleep)
+    if cancel_signal != True:
+        sleep_time = 1/float(freq)
+        threading.Timer(sleep_time, remote_interface).start()
+
+remote_interface()
+# r = rospy.Rate(freq)
+# while not rospy.is_shutdown():
+#     # interface()
+#     r.sleep()
